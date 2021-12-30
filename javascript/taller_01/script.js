20abeda058544a66f2fad03095a19bf5805f0562
sbravoa1 <samuelbrav@gamil.com>
2021-12-30 03:34:01 -0500

    Javascript-basic / tipo de datos

--- javascript/taller_01/script.js
+++ javascript/taller_01/script.js
@@ -1,2 +1,47 @@
-var a=0
-console.log( typeof a)
+// Tipado dinamico
+var a = 42
+console.log( a )
+
+a = 'Hola mundo'
+console.log( a )
+
+a = true
+console.log( a )
+
+// Tipos de datos primitivos
+var a = 2
+var b = 3.2
+console.log( typeof a )
+console.log( typeof b )
+
+var c = true
+console.log( typeof c )
+
+var d = 'Hola mundo'
+console.log( typeof d )
+
+var e
+console.log( typeof e )
+console.log( e )
+
+var d = null
+console.log( typeof d )
+console.log( d )
+
+var x = 2n ** 53n
+console.log( typeof x )
+
+console.log( x )
+
+// objetos
+var persona = {
+    nombre: 'Samuel',
+    apellido: 'Bravo',
+    ciudad: 'Guayaquil'
+}
+
+console.log( persona )
+console.log( typeof persona )
+
+persona.nombre = 'Samuel Santiago'
+console.log( persona )
